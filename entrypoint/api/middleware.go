47deb2f50bace3f33f1b4eb9cc205e95cd2e49e5
akiho <akiho@akiho-macbookpro.local>
2018-07-16 19:35:28 +0900

    fixed middleware

--- entrypoint/api/middleware.go
+++ entrypoint/api/middleware.go
@@ -27,41 +27,37 @@ func auth(base http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 
-		uid := r.Header.Get("X-Debug-User-Id")
-		newContext, _ := contextUtil.WithAuthUID(ctx, models.UserID(uid))
-		base.ServeHTTP(w, r.WithContext(newContext))
+		//ローカルで起動した場合はアクセストークンではなくuidをつけてリクエストする
+		//X-Debug-User-Id: l7W16MD993NuV9IGJYpRJxkVE543
+		if appengine.IsDevAppServer() {
+			uid := r.Header.Get("X-Debug-User-Id")
+			newContext, _ := contextUtil.WithAuthUID(ctx, models.UserID(uid))
+			base.ServeHTTP(w, r.WithContext(newContext))
+		} else {
+			client := utils.NewFirebaseUtil().InitAuthClient(ctx)
 
-		// ローカルで起動した場合はアクセストークンではなくuidをつけてリクエストする
-		// X-Debug-User-Id: l7W16MD993NuV9IGJYpRJxkVE543
-		//if appengine.IsDevAppServer() {
-		//	uid := r.Header.Get("X-Debug-User-Id")
-		//	newContext, _ := contextUtil.WithAuthUID(ctx, models.UserID(uid))
-		//	base.ServeHTTP(w, r.WithContext(newContext))
-		//} else {
-		//	client := utils.NewFirebaseUtil().InitAuthClient(ctx)
-		//
-		//	token := r.Header.Get("Authorization")
-		//	if len(token) <= 7 {
-		//		w.WriteHeader(403)
-		//		return
-		//	}
-		//
-		//	decoded, err := client.VerifyIDToken(token[7:])
-		//	if err != nil {
-		//		w.WriteHeader(403)
-		//		return
-		//	}
-		//
-		//	newContext, _ := contextUtil.WithAuthUID(ctx, models.UserID(decoded.UID))
-		//	base.ServeHTTP(w, r.WithContext(newContext))
-		//}
+			token := r.Header.Get("Authorization")
+			if len(token) <= 7 {
+				w.WriteHeader(403)
+				return
+			}
+
+			decoded, err := client.VerifyIDToken(token[7:])
+			if err != nil {
+				w.WriteHeader(403)
+				return
+			}
+
+			newContext, _ := contextUtil.WithAuthUID(ctx, models.UserID(decoded.UID))
+			base.ServeHTTP(w, r.WithContext(newContext))
+		}
 	})
 }
 
 func cros(base http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(204)
